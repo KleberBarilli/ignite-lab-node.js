acd81eca1d5ae7790eb2d8ad35b1b25e2333d0bd
kleber.barilli <kleber.barilli73@gmail.com>
2022-12-13 21:51:06 -0300

    add getters e setters

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -12,12 +12,35 @@ export class Notification {
   constructor(data: INotification) {
     this.data = data;
   }
+  public get recipientId(): string {
+    return this.data.recipientId;
+  }
 
-  public set content(content: string) {
-    this.data.content = content;
+  public set recipientId(content: string) {
+    this.data.recipientId = this.recipientId;
   }
 
   public get content(): string {
     return this.data.content;
+  }
+  public set content(content: string) {
+    this.data.content = content;
+  }
+  public get category(): string {
+    return this.data.category;
+  }
+  public set category(category: string) {
+    this.data.category = category;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.data.readAt;
+  }
+  public set readAt(readAt: Date | null | undefined) {
+    this.data.readAt = this.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.data.createdAt;
   }
 }
