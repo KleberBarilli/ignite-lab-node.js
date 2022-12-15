643d6ed7fc2e0cefd8be220c660e9b4a33359348
kleber.barilli <kleber.barilli73@gmail.com>
2022-12-14 22:18:18 -0300

    ts paths

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { SendNotification } from "src/app/use-cases/send-notification";
 import { CreateNotificationBody } from "../dtos/create-notification-body";
 
