ddf70ad86ba9dcc7afd230dc2e2db55afdf309dd
kleber.barilli <kleber.barilli73@gmail.com>
2022-12-15 22:58:47 -0300

    add kafka

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleDestroy } from "@nestjs/common";
+import { ServerKafka } from "@nestjs/microservices";
+
+@Injectable()
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  constructor() {
+    super({
+      client: {
+        clientId: "notifications",
+        brokers: ["sought-javelin-6618-us1-kafka.upstash.io:9092"],
+        sasl: {
+          mechanism: "scram-sha-256",
+
+          username: process.env.KAFKA_USERNAME || "",
+
+          password: process.env.KAFKA_PASSWORD || "",
+        },
+        ssl: true,
+      },
+    });
+  }
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
---
+++ src/infra/messaging/messaging.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { KafkaConsumerService } from "./kafka/kafka-consumer.service";
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [KafkaConsumerService],
+})
+export class MessagingModule {}
