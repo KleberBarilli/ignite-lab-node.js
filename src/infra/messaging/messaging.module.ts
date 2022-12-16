c2f93069641f8af8365a6769998f7933886cfce6
kleber.barilli <kleber.barilli73@gmail.com>
2022-12-15 23:16:31 -0300

    add send notification with kafka

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,9 +1,12 @@
+import { SendNotification } from "@app/use-cases/send-notification";
+import { DatabaseModule } from "@infra/database/database.module";
 import { Module } from "@nestjs/common";
+import { NotificationsController } from "./kafka/controllers/notifications.controller";
 import { KafkaConsumerService } from "./kafka/kafka-consumer.service";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [KafkaConsumerService],
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [KafkaConsumerService, SendNotification],
 })
 export class MessagingModule {}
