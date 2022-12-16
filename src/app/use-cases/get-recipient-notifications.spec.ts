c9b0644fb0610e0ffa54f0c962b12878d8dbcf43
kleber.barilli <kleber.barilli73@gmail.com>
2022-12-15 22:15:19 -0300

    fix cancel at

--- src/app/use-cases/get-recipient-notifications.spec.ts
+++ src/app/use-cases/get-recipient-notifications.spec.ts
@@ -1,5 +1,3 @@
-import { Content } from "@app/entities/content";
-import { Notification } from "@app/entities/notification";
 import { makeNotification } from "@app/repositories/factories/notification-factory";
 import { InMemoryNotificationsRepository } from "../repositories/fakes/in-memory-notifications-repository";
 import { GetRecipientNotifications } from "./get-recipient-notifications";
